a9fe0602bdd2b3399ac7fe52850ab86588288e87
Illya <ilya.solovev.88@bk.ru>
2023-07-11 16:41:38 +0300

    feature closing other headings in an accordion module

--- dist/script.js
+++ dist/script.js
@@ -16,13 +16,21 @@ const accordion = triggersSelector => {
   const btns = document.querySelectorAll(triggersSelector);
   btns.forEach(btn => {
     btn.addEventListener('click', function () {
-      this.classList.toggle('active-style');
-      this.nextElementSibling.classList.toggle('active-content');
-      if (this.classList.contains('active-style')) {
-        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + '80px';
-      } else {
-        this.nextElementSibling.style.maxHeight = '0px';
-      }
+      btns.forEach(item => {
+        if (item !== this) {
+          item.classList.remove('active-style');
+          item.nextElementSibling.classList.remove('active-content');
+          item.nextElementSibling.style.maxHeight = '0px';
+        } else {
+          this.classList.toggle('active-style');
+          this.nextElementSibling.classList.toggle('active-content');
+          if (this.classList.contains('active-style')) {
+            this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + '80px';
+          } else {
+            this.nextElementSibling.style.maxHeight = '0px';
+          }
+        }
+      });
     });
   });
 };
--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -3,14 +3,22 @@ const accordion = (triggersSelector) => {
 
     btns.forEach(btn => {
         btn.addEventListener('click', function() {
-            this.classList.toggle('active-style');
-            this.nextElementSibling.classList.toggle('active-content');
-
-            if (this.classList.contains('active-style')) {
-                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + '80px';
-            } else {
-                this.nextElementSibling.style.maxHeight = '0px';
-            }
+            btns.forEach(item => {
+                if (item !== this) {
+                    item.classList.remove('active-style');
+                    item.nextElementSibling.classList.remove('active-content');
+                    item.nextElementSibling.style.maxHeight = '0px';
+                } else {
+                    this.classList.toggle('active-style');
+                    this.nextElementSibling.classList.toggle('active-content');
+        
+                    if (this.classList.contains('active-style')) {
+                        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + '80px';
+                    } else {
+                        this.nextElementSibling.style.maxHeight = '0px';
+                    }
+                }
+            });
         });
     });
 };
